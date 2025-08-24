4bae1f8b9f2ce07cfa39644ccbbe08884b606b83
Куликов Валерий Романович <VRKulikov@beeline.ru>
2025-08-24 21:49:11 +0300

    HW5

---
+++ src/main/java/org/example/Article.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "article_seq")
+    @SequenceGenerator(name = "article_seq", sequenceName = "article_seq", allocationSize = 50)
+    private Long id;
+
+    @Column(nullable = false, length = 1000)
+    @NotBlank
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private Instant updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false, foreignKey = @ForeignKey(name = "fk_articles_author"))
+    private Author author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(getId(), article.getId()) && Objects.equals(getContent(), article.getContent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getContent());
+    }
+}
---
+++ src/main/java/org/example/Author.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(
+        name = "authors",
+        indexes = {
+                @Index(name = "ax_authors_name", columnList = "name", unique = true)
+        }
+)
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_seq")
+    @SequenceGenerator(name = "author_seq", sequenceName = "author_seq", allocationSize = 50)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    @Size(min = 3, max = 30)
+    @Pattern(regexp = "^[A-Za-z0-9_]+$")
+    private String name;
+
+    @OneToMany(
+            mappedBy = "author",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    private List<Article> articles = new ArrayList<>();
+
+    public void addArticle(Article article) {
+        articles.add(article);
+        article.setAuthor(this);
+    }
+
+    public void removeArticle(Article article) {
+        articles.remove(article);
+        article.setAuthor(null);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author other = (Author) o;
+
+        return id != null && id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", name='" + name + "'}";
+    }
+}
